bfa6ef3f6d28767535242c1843184902e516f821
Patsy Price <patsy.m.price@gmail.com>
2013-07-18 22:44:26 -0700

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherPlayground::Application.config.secret_token = 'b19147889b115b3f169572a290edd771d9ed0c3202783f845e8fdd2e3d0697ddc8ab9ad6ab4791024346b2645d18b5ca6cdbaf26c9cfa08ee77e90b340dbb27f'
